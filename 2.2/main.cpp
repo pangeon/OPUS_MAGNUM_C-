a13a203108c74d1988360115c5c05e20a781b25a
Kamil Cecherz <kamil.cecherz@gmail.com>
2022-05-14 20:31:06 +0200

    add comments

--- 2.2/main.cpp
+++ 2.2/main.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 int main()
 {
-    string your_name;
-    int points = 0;
+    string your_name; // zawiera napis
+    int points = 0; // zawiera liczbę całkowitą 
 
     cout << "Podaj swoje imie: ";
     cin >> your_name;
 
-    if(your_name == "Anna")
+    if (your_name == "Anna")
     {
         cout << "Dobrze !" << endl;
-        points++;
+        points++; // inkremetacja zw. o 1
         cout << "Twoje punkty: " << points << endl;
-    } 
-    else 
+    }
+    else
     {
         cout << "Twoje imie nie brzmi: " << your_name << endl;
-        points--;
+        points--; // dekrementacja zmniejszo o 1
         cout << "Twoje punkty: " << points << endl;
     }
 }
